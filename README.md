d471ea1400605d00e7df5bf1b5555172531b3d2c
HanhLe <huuhanhle1997@gmail.com>
2019-05-11 23:12:08 +0700

    RNNtheano

---
+++ README.md
@@ -0,0 +1,8 @@
+﻿theano-rnn
+=====
+
+demo bằng theano dùng để dự đoán kết truyện
+đầu vào là 1 cuốn truyện
+đầu ra là kết truyện của cuốn truyện đó
+data gồm 2 truyện với dung lượng 4.28 MB
+kết quả chính xác khoảng 0.86
---
+++ util.py
@@ -0,0 +1,44 @@
+import pickle
+import sys
+
+import numpy as np
+
+from models import gru
+from models import lstm
+
+def onehot(sequence, char_to_ix):
+    seq_oh = np.zeros((len(sequence), len(char_to_ix)))
+    for i,x in enumerate(sequence):
+        seq_oh[i][char_to_ix[x]] = 1
+    return seq_oh
+
+def save_model(f, model, char_to_ix, ix_to_char):
+    ps = {}
+    ps['type'] = model.type
+    ps['in_size'] = model.in_size
+    ps['out_size'] = model.out_size
+    ps['layers'] = model.layers
+    ps['rnn_size'] = model.rnn_size
+    ps['char_to_ix'] = char_to_ix
+    ps['ix_to_char'] = ix_to_char
+    for w in model.weights():
+        ps[w.name] = w.get_value()
+    pickle.dump(ps, open(f, 'wb'))
+
+def load_model(f, model='', is_train=1):
+    try:
+        ps = pickle.load(open(f, 'rb'))
+    except:
+        print('file not found')
+        sys.exit(1)
+    if model == '' and ps['type'] == 'lstm':
+        model = lstm.lstm(ps['in_size'], ps['rnn_size'], ps['out_size'],
+                          ps['layers'], is_train=is_train)
+    elif model == '' and ps['type'] == 'gru':
+        model = gru.gru(ps['in_size'], ps['rnn_size'], ps['out_size'],
+                        ps['layers'], is_train=is_train)
+    model.char_to_ix = ps['char_to_ix']
+    model.ix_to_char = ps['ix_to_char']
+    model.load_weights(ps)
+    return model
+
